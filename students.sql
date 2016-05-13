847f415fd0017f1faaa946b1724844ee335798c6
someApprentice <oidfg@mail.ru>
2016-05-13 09:10:02 +0700

    Заменен дамп БД на актуальный

--- students.sql
+++ students.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Хост: 127.0.0.1
--- Время создания: Фев 18 2016 г., 09:33
+-- Время создания: Май 13 2016 г., 04:08
 -- Версия сервера: 5.6.21
 -- Версия PHP: 5.6.3
 
@@ -31,15 +31,15 @@ CREATE TABLE IF NOT EXISTS `students` (
   `name` varchar(20) COLLATE utf8_bin NOT NULL,
   `surname` varchar(20) COLLATE utf8_bin NOT NULL,
   `gender` enum('Man','Woman') COLLATE utf8_bin NOT NULL,
-  `grupnumber` varchar(5) COLLATE utf8_bin NOT NULL,
+  `grupNumber` varchar(5) COLLATE utf8_bin NOT NULL,
   `email` varchar(255) COLLATE utf8_bin NOT NULL,
-  `satscores` int(11) NOT NULL,
-  `yearofbirth` year(4) NOT NULL,
+  `satScores` int(11) NOT NULL,
+  `yearOfBirth` year(4) NOT NULL,
   `location` enum('local','nonresident') COLLATE utf8_bin NOT NULL,
-  `password` varchar(32) COLLATE utf8_bin NOT NULL,
+  `hash` varchar(32) COLLATE utf8_bin NOT NULL,
   `salt` varchar(44) COLLATE utf8_bin NOT NULL,
   `token` varchar(32) COLLATE utf8_bin NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=31 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+) ENGINE=InnoDB AUTO_INCREMENT=107 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
 --
 -- Индексы сохранённых таблиц
@@ -59,7 +59,7 @@ ALTER TABLE `students`
 -- AUTO_INCREMENT для таблицы `students`
 --
 ALTER TABLE `students`
-MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=31;
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=107;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
