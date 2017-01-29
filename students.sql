25f8ee94607067597c5be7c18c966e26095938be
someApprentice <someApprentice.openmailbox.org>
2017-01-29 13:55:57 +0700

    Кодировка таблиц заменена на utf8_unicode_ci

--- students.sql
+++ students.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Хост: 127.0.0.1
--- Время создания: Июл 12 2016 г., 11:52
+-- Время создания: Янв 29 2017 г., 07:53
 -- Версия сервера: 5.6.21
 -- Версия PHP: 5.6.3
 
@@ -28,17 +28,17 @@ SET time_zone = "+00:00";
 
 CREATE TABLE IF NOT EXISTS `students` (
 `id` int(11) NOT NULL,
-  `name` varchar(20) COLLATE utf8_bin NOT NULL,
-  `surname` varchar(20) COLLATE utf8_bin NOT NULL,
-  `gender` enum('Man','Woman') COLLATE utf8_bin NOT NULL,
-  `grupNumber` varchar(5) COLLATE utf8_bin NOT NULL,
-  `email` varchar(255) COLLATE utf8_bin NOT NULL,
+  `name` varchar(20) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+  `surname` varchar(20) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+  `gender` enum('Man','Woman') CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+  `grupNumber` varchar(5) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+  `email` varchar(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
   `satScores` int(11) NOT NULL,
   `yearOfBirth` year(4) NOT NULL,
-  `location` enum('local','nonresident') COLLATE utf8_bin NOT NULL,
-  `hash` varchar(32) COLLATE utf8_bin NOT NULL,
-  `salt` varchar(44) COLLATE utf8_bin NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=116 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+  `location` enum('local','nonresident') CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+  `hash` varchar(32) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+  `salt` varchar(44) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=118 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
 --
 -- Индексы сохранённых таблиц
@@ -58,7 +58,7 @@ ALTER TABLE `students`
 -- AUTO_INCREMENT для таблицы `students`
 --
 ALTER TABLE `students`
-MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=116;
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=118;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
